5a3988ca11e05e241e2b823c737bcade5041fb93
ritikadhoot <ritikadhoot314@gmail.com>
2017-06-28 13:06:22 +0530

    Add files via upload

---
+++ soc/config.py
@@ -0,0 +1,34 @@
+DB_NAME_DEFAULT = 'soc'                                                                      
+DB_USER_DEFAULT = ''
+                                                                     
+DB_PASS_DEFAULT = ''   
+                                                              
+DB_HOST_DEFAULT = ''                                                                         
+DB_PORT_DEFAULT = ''                                                                         
+
+
+DB_NAME_SCILAB = ''
+
+DB_USER_SCILAB = ''
+                                                                      
+DB_PASS_SCILAB = '' 
+                                                                 
+DB_HOST_SCILAB = ''                                                                          
+DB_PORT_SCILAB = ''                                                                          
+
+BIN = '/home/scilab_test/CLOUD/scilab/scilab-5.4.1/bin/scilab-adv-cli'
+#SCILAB_FLAGS = '-noatomsautoload'
+SCILAB_FLAGS = '-noatomsautoload -nogui -nb ' #srikant
+SCIMAX_LOADER = '/home/scilab_test/CLOUD/scilab/scilab-scimax-2.1.4/loader.sce'
+UPLOADS_PATH = '/home/scilab_test/CLOUD/uploads            
+
+EMAIL_HOST = ''
+EMAIL_PORT = 
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = ''
+EMAIL_USE_TLS = True                                                              
+
+SCILAB_3= 'scilab-5.3.3'
+SCILAB_4= 'scilab-5.4.1'
+SCILAB_5= 'scilab-5.5.2'
+
